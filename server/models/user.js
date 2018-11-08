27db350f3953c160c4c5c6f1461e71f05c840a07
Brendan Cottenham <BrendanCottenham@outlook.com>
2018-11-08 14:28:07 -0500

    Add POST /users login route

--- server/models/user.js
+++ server/models/user.js
@@ -10,7 +10,7 @@ var UserSchema = new mongoose.Schema({
     required: true,
     trim: true,
     minlength: 1,    
-    //unique: false, //This line is causing the crashing tests
+    unique: true, //This line is causing the crashing tests
     validate: {
       validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
@@ -66,6 +66,27 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
